8064fd0f13d0deb2856327ec2ce6c3dd52614181
Drummberg <drummberg@gmail.com>
2022-01-28 00:49:03 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,6 +8,10 @@ const player = new vimeoPlayer(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
+if (localStorage.getItem(CURRENT_TIME)) {
+  player.setCurrentTime(Number.parseFloat(localStorage.getItem(CURRENT_TIME)))
+}
+
 
 function onPlay(event) {
   localStorage.setItem(CURRENT_TIME, event.seconds === event.duration ? 0 : event.seconds)
