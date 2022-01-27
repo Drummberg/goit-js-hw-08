f9e18f2e995515cee4277a04994ac5eaa6a6cc98
Drummberg <drummberg@gmail.com>
2022-01-27 22:15:21 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,12 @@
 import throttle from 'lodash.throttle'
 
-
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
+
+populateForm();
 
 function onFormInput(evt) {
     const formData = { email: form.email.value, message: form.message.value };
@@ -18,7 +19,31 @@ function onFormInput(evt) {
 function onFormSubmit(evt) {
     evt.preventDefault();
 
+    if (evt.target.email.value === '') {
+    alert('Please enter your email')
+    return
+    }
+    if (evt.target.message.value === '') {
+    alert('Please type your message')
+    return
+    }
+
+    const formDataSubmit = { email: form.email.value, message: form.message.value };
+
+    console.log(formDataSubmit);
+
     evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
 
 }
 
+function populateForm() {
+    const savedTextForm = localStorage.getItem(STORAGE_KEY);
+    const savedTextFormParsed = JSON.parse(savedTextForm);
+
+    if (savedTextFormParsed) {
+        form.email.value = savedTextFormParsed.email;
+        form.message.value = savedTextFormParsed.message;
+    }
+    
+}
