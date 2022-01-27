ce3ae5428fc8923ba03b17bb3cb9302a6e7e6804
Drummberg <drummberg@gmail.com>
2022-01-27 21:17:01 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle'
+
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+    const formData = { email: form.email.value, message: form.message.value };
+
+    const formDataStringify = JSON.stringify(formData);
+    
+    localStorage.setItem(STORAGE_KEY, formDataStringify);
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+
+}
 
