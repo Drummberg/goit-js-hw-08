79662451f3b3e2e97f55f8afe2cd79c36cb21485
Drummberg <drummberg@gmail.com>
2022-01-28 00:44:00 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
 
+import throttle from 'lodash.throttle'
+import vimeoPlayer from '@vimeo/player'
+
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+function onPlay(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds === event.duration ? 0 : event.seconds)
+}
